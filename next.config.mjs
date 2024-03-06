1a5d94f63f22620f2fcf3f0939d1bbee1622c761
imaditya123 <imadityarathore@gmail.com>
2024-03-07 03:01:48 +0530

    fix:purge issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./Components/**/*.{js,ts,jsx,tsx}"],
     output: "export",
     reactStrictMode: true,
     basePath: '/github-pages',
-
-
 };
 
 export default nextConfig;
