1ac8ad8a00ea876168366f28aaf620967c788fa1
imaditya123 <imadityarathore@gmail.com>
2024-03-15 04:53:09 +0530

    fix: github pages1

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,24 @@
+// /** @type {import('next').NextConfig} */
+
+// const nextConfig = {
+//     output: "export",
+//     reactStrictMode: true,
+//     skipTrailingSlashRedirect: true,
+
+//     // basePath: '/github-pages',
+//     // purge: ['./pages/**/*.{js,ts,jsx,tsx}',
+//     // './components/**/*.{js,ts,jsx,tsx}',
+//     // './src/components/**/*.{js,ts,jsx,tsx}',
+//     // './src/section/**/*.{js,ts,jsx,tsx}',
+
+// // ],
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    reactStrictMode: true,
-    basePath: '/github-pages',
-    purge: ['./pages/**/*.{js,ts,jsx,tsx}', 
-    './components/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/section/**/*.{js,ts,jsx,tsx}',
-
-],
+  output: "export",
 };
-
+/* module.exports = nextConfig*/
 export default nextConfig;
